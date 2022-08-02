41edc5a0ff5910bc65489b6615b73632e62cf56a
krishnarayapudi25 <95125632+krishnarayapudi25@users.noreply.github.com>
2022-08-02 12:11:38 +0530

    Added my project

---
+++ src/main/java/com/sample_maven/docker_publish/DockerPublishApplication.java
@@ -0,0 +1,13 @@
+package com.sample_maven.docker_publish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPublishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerPublishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample_maven/docker_publish/DockerPublishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample_maven.docker_publish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPublishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
